38204f8acae88abaefe44b1a5c613bc47b5701c6
w0rng <antonabramov2000@gmail.com>
2021-01-23 17:58:11 +0700

    ADD setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+setup(
+    name='sqlalchemy_basemodel',
+    version='0.1',
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README.md')).read(),
+    install_requires=[
+        'SQLAlchemy>=1.3.22'
+    ]
+)
